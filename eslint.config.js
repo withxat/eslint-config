8388b50532c1152eecb059bde65d7511b4e08ea4
Xat <i@xat.sh>
2025-04-01 23:28:04 +0800

    chore: use js type eslint config

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import styleMigrate from '@stylistic/eslint-plugin-migrate'
+
+// eslint-disable-next-line antfu/no-import-dist
+import { xat } from './dist/index.js'
+
+export default xat(
+	{
+		vue: {
+			a11y: true,
+		},
+		react: true,
+		solid: true,
+		svelte: true,
+		astro: true,
+		typescript: true,
+		formatters: true,
+		pnpm: true,
+		type: 'lib',
+	},
+	{
+		ignores: [
+			'_fixtures',
+			'**/constants-generated.ts',
+		],
+	},
+	{
+		files: ['src/**/*.ts'],
+		rules: {
+			'perfectionist/sort-objects': 'error',
+		},
+	},
+	{
+		files: ['src/configs/*.ts'],
+		plugins: {
+			'style-migrate': styleMigrate,
+		},
+		rules: {
+			'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
+		},
+	},
+)
