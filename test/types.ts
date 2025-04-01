e4f24fc1e08734b791e20ca1bea2e44a47c75078
Xat <i@xat.sh>
2025-04-01 23:27:11 +0800

    build: rm test

--- test/types.ts
+++
@@ -1,7 +0,0 @@
-import type { TypedFlatConfigItem } from '@/types'
-
-import type { Linter } from 'eslint'
-
-// Make sure they are compatible
-((): Linter.Config => ({} as TypedFlatConfigItem))();
-((): TypedFlatConfigItem => ({} as Linter.Config))()
