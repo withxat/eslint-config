9bc510cc97f0edf4b6a6ea984f58a0bbc614e7b5
Xat <i@xat.sh>
2025-04-02 21:19:59 +0800

    chore: cleanup eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,8 @@
-import styleMigrate from '@stylistic/eslint-plugin-migrate'
-
 // eslint-disable-next-line antfu/no-import-dist
 import { xat } from './dist/index.js'
 
 export default xat(
 	{
-		formatters: true,
-		pnpm: true,
 		type: 'lib',
-	},
-	{
-		ignores: [
-			'_fixtures',
-			'**/constants-generated.ts',
-		],
-	},
-	{
-		files: ['src/**/*.ts'],
-		rules: {
-			'perfectionist/sort-objects': 'error',
-		},
-	},
-	{
-		files: ['src/configs/*.ts'],
-		plugins: {
-			'style-migrate': styleMigrate,
-		},
-		rules: {
-			'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
-		},
 	},
 )
